325f96d41c18de71ed435d02f9f05a8b6f3bee04
Vend Travis Builder <admin+vend_travis_builder@vendhq.com>
2018-02-21 00:49:06 +0000

    From vend-api-2.0-specification: Some strangely missing sale properties

--- src/main/java/com/vendhq/models/ConsignmentCollection.java
+++ src/main/java/com/vendhq/models/ConsignmentCollection.java
@@ -14,6 +14,7 @@
 package com.vendhq.models;
 
 import java.util.Objects;
+import java.util.Arrays;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
