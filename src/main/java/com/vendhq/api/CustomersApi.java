3c36b0e13f39f5f18a43198b4f561bc1a40e4744
Vend Travis Builder <admin+vend_travis_builder@vendhq.com>
2018-12-17 03:33:08 +0000

    From vend-api-2.0-specification: add channel request log endpoints and models

--- src/main/java/com/vendhq/api/CustomersApi.java
+++ src/main/java/com/vendhq/api/CustomersApi.java
@@ -167,10 +167,11 @@ public class CustomersApi {
    * @param after The lower limit for the version numbers to be included in the response. (optional)
    * @param before The upper limit for the version numbers to be included in the response. (optional)
    * @param pageSize The maximum number of items to be returned in the response. (optional)
+   * @param deleted Indicates whether deleted items should be included in the response. (optional)
    * @return CustomerCollection
    * @throws ApiException if fails to make API call
    */
-  public CustomerCollection listCustomers(Long after, Long before, Integer pageSize) throws ApiException {
+  public CustomerCollection listCustomers(Long after, Long before, Integer pageSize, Boolean deleted) throws ApiException {
     Object localVarPostBody = null;
     
     // create path and map variables
@@ -184,6 +185,7 @@ public class CustomersApi {
     localVarQueryParams.addAll(apiClient.parameterToPairs("", "after", after));
     localVarQueryParams.addAll(apiClient.parameterToPairs("", "before", before));
     localVarQueryParams.addAll(apiClient.parameterToPairs("", "page_size", pageSize));
+    localVarQueryParams.addAll(apiClient.parameterToPairs("", "deleted", deleted));
 
     
     
